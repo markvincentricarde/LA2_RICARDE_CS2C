bec202053b59e085e3f01627c8811aa1685b0df1
markvincentricarde <vincentricarde@gmail.com>
2024-09-25 00:03:51 +0800

    initial commit

---
+++ CS2C_MarkVincentRicarde_C1-A1.js
@@ -0,0 +1,11 @@
+let pName1 = "Jess Callego Avillano";
+let pBirthdate1 = "December 3,2005";
+let pBirthplace = "City of Candon Hospital,Candon";
+let pAddress1 = "Sabangan,Launion,Philippines,2072";
+let pCource1 = "bachelor of Science in Computer Science";
+let pDreamjob1 = "Web Developer";
+let up = pName1.toHigherCase();
+let down = pName1.toLowerCase();
+
+let send = up + "was born " + pBirthdate1 + "at" + pBirthplace + ", and currently living at" + destine + "," + down + "is taking up" + course + "and dreams to be" + dream + "after graduation";
+console.log(send);
---
+++ CS2C_MarkVincentRicarde_C1-A2.js
@@ -0,0 +1,24 @@
+//Matheatical and Compareson Operation 
+
+let a = 25;
+let b = 20;
+let c = 12;
+let d = "15";
+const e = 15;
+
+let sum = (25 + 20 + 12 + "15" +15);
+console.log(sum);
+
+console.log("15" > 15);
+console.log("15" < 15);
+console.log("15" >= 15);
+console.log("15" <= 15);
+console.log("15" === 15);
+
+let combine = (a-b-c-d-e);
+let connect = ((a** c)/ e); 
+let join = (e** c);
+
+console.log(a-b-c-d-e);
+console.log((a** c)/ e);
+console.log(e** c);
